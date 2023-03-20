0d21b8ff536c5c97d6bea3a69df1afd18f5f19c3
Evaisa <evagiacosa1@gmail.com>
2023-03-20 17:10:56 +0100

    switch health system, clueless

--- evaisa.arena/files/scripts/gamemode/misc/kill_check.lua
+++ evaisa.arena/files/scripts/gamemode/misc/kill_check.lua
@@ -8,18 +8,37 @@ function damage_about_to_be_received( damage, x, y, entity_thats_responsible, cr
         local health = ComponentGetValue2( damageModelComponent, "hp" )
         if(GameHasFlagRun("Immortal"))then
             return 0, 0
-        else
-            GameAddFlagRun("took_damage")
+        --else
+        --    GameAddFlagRun("took_damage")
         end
-        if health - damage <= 0 then
+        --[[if health - damage <= 1 then
             GameAddFlagRun("player_died")
             if(entity_thats_responsible ~= nil)then
                 ModSettingSet("killer", EntityGetName(entity_thats_responsible))
             end
             return 0, 0
-        end
+        end]]
     end
 
 
     return damage, 0
+end
+
+function damage_received( damage, message, entity_thats_responsible, is_fatal, projectile_thats_responsible )
+    local entity_id = GetUpdatedEntityID()
+    
+    -- check if would kill
+    GameAddFlagRun("took_damage")
+
+    local damageModelComponent = EntityGetFirstComponentIncludingDisabled( entity_id, "DamageModelComponent" )
+    if damageModelComponent ~= nil then
+        if(is_fatal)then
+            GameAddFlagRun("player_died")
+            if(entity_thats_responsible ~= nil)then
+                ModSettingSet("killer", EntityGetName(entity_thats_responsible))
+            end
+            -- set health so that player ends on 1
+            ComponentSetValue2( damageModelComponent, "hp", damage + 1 )
+        end
+    end
 end
