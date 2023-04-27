c768aae3f7667f42d57ae9040d47664407a65421
Evaisa <evagiacosa1@gmail.com>
2023-04-27 21:47:57 +0200

    switch back to item pick up api function

--- evaisa.arena/files/scripts/utilities/EZWand.lua
+++ evaisa.arena/files/scripts/utilities/EZWand.lua
@@ -1091,18 +1091,25 @@ function wand:PickUp(entity)
   if item_component then
     ComponentSetValue2(item_component, "has_been_picked_by_player", true)
   end
-  --GamePickUpInventoryItem(entity, self.entity_id, false)
-  local entity_children = EntityGetAllChildren(entity) or {}
+  GamePickUpInventoryItem(entity, self.entity_id, false)
+  --local entity_children = EntityGetAllChildren(entity) or {}
   -- 
-  for key, child in pairs( entity_children ) do
+  --[[for key, child in pairs( entity_children ) do
     if EntityGetName( child ) == "inventory_quick" then
       EntityAddChild( child, self.entity_id )
     end
-  end
+  end]]
 
   EntitySetComponentsWithTagEnabled( self.entity_id, "enabled_in_world", false )
   EntitySetComponentsWithTagEnabled( self.entity_id, "enabled_in_hand", false )
   EntitySetComponentsWithTagEnabled( self.entity_id, "enabled_in_inventory", true )
+
+  local wand_children = EntityGetAllChildren(self.entity_id) or {}
+
+  for k, v in ipairs(wand_children)do
+    EntitySetComponentsWithTagEnabled( self.entity_id, "enabled_in_world", false )
+  end
+
 end
 
 -- Turns the wand properties etc into a string
