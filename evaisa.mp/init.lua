ac4c42798b66baa65e80eb76412f3c317a5fbde6
Evaisa <evagiacosa1@gmail.com>
2023-10-09 17:02:38 +0200

    Update init.lua

--- evaisa.mp/init.lua
+++ evaisa.mp/init.lua
@@ -133,7 +133,7 @@ delay = dofile("mods/evaisa.mp/lib/delay.lua")
 
 popup = dofile("mods/evaisa.mp/files/scripts/popup.lua")
 
-MP_VERSION = 1.6	
+MP_VERSION = 1.61	
 VERSION_FLAVOR_TEXT = "$mp_beta"
 noita_online_download = "https://github.com/EvaisaDev/noita-online/releases"
 Version_string = "63479623967237"
